9ea5bd2e1ef08fcff6312b006ed0eea7d837daf3
Matty <54020396+matty-ross@users.noreply.github.com>
2026-07-29 20:46:11 +0200

    Moved enums from game actions to types.

--- include/bpr/GameActions.hpp
+++ include/bpr/GameActions.hpp
@@ -3,6 +3,8 @@
 
 #include <cstdint>
 
+#include "bpr/Types.hpp"
+
 
 namespace BPR
 {
@@ -32,54 +34,6 @@ namespace BPR
 
         return result;
     }
-
-
-    enum class VehicleType
-    {
-        Invalid = -1,
-        Car = 0,
-        Motorbike = 1,
-        Plane = 2,
-    };
-
-    enum class BoostType
-    {
-        Speed = 0,
-        Aggression = 1,
-        Stunt = 2,
-        None = 3,
-        Locked = 4,
-    };
-
-    /*
-        enum BrnWorld::CarControl
-    */
-    enum class PlayerVehicleDriver
-    {
-        None = 0,
-        Player = 1,
-        AI = 2,
-    };
-
-    /*
-        enum BrnPhysics::Deformation::DeformationResetType
-    */
-    enum class DeformationType
-    {
-        None = -1,
-        Event = 0,
-        VehicleSelect = 1,
-    };
-
-    /*
-        enum BrnGameState::GameStateModuleIO::ResetPlayerCarAction::CarSelectType
-    */
-    enum class VehicleSelectType
-    {
-        DontDrop = 0,
-        DropNormal = 1,
-        DropShutdown = 2,
-    };
 
 
     /*
